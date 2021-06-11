a972f4c80e1d784c2ea8f78e363ba9794ef071d1
kimgri <kim.gripenberg@paf.com>
2021-06-11 10:12:25 +0300

    Removed hardcoded path
    potatoe brain stuck again

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,9 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   
-  plugins: ["C:/Users/kimgri/blog/plugins/sanity-blocks.js",'@plugins/sanity-blocks.js'],
+  plugins: [
+    
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
